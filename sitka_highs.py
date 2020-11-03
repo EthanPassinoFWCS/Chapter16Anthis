b63799117e52677334b12d3d5794a3a86819da5d
EthanPassinoFWCS <ethan.passino@fwcsstudents.org>
2020-11-03 13:15:54 -0500

    Adding in dates.

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,5 @@
 import csv
-
+from datetime import datetime
 import matplotlib.pyplot as plt
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
@@ -7,21 +7,25 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    # Get high temperatures from this file.
-    highs = []
+    # Get dates and high temperatures from this file.
+    dates, highs = [], []
+
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
     # Making the highs into a graph.
 
     plt.style.use('seaborn')
     fig, ax = plt.subplots()
-    ax.plot(highs, c='red')
+    ax.plot(dates, highs, c='red')
 
     # Format the plot.
     plt.title("Daily high temperatures, July 2018", fontsize=24)
     plt.xlabel("", fontsize=16)
+    fig.autofmt_xdate()
     plt.ylabel("Temperature (F)", fontsize=16)
     plt.tick_params(axis='both', which='major', labelsize=16)
 
