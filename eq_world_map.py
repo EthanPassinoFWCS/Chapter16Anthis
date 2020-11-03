ebf6962e8740d55610286bb43c7e95d3dd9fdbf1
EthanPassinoFWCS <ethan.passino@fwcsstudents.org>
2020-11-03 13:52:27 -0500

    Created an offline html page file with the data.

---
+++ eq_world_map.py
@@ -0,0 +1,35 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+# Explore the structure of the data
+filename = 'data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']  # Getting the magnitude of the earthquake from our data.
+    lon = eq_dict['geometry']['coordinates'][0]  # gets the longitude
+    lat = eq_dict['geometry']['coordinates'][1]  # gets the latitude
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+
+# Map the earthquakes
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'marker': {
+        'size': [5*mag for mag in mags],
+    },
+}]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='htmls/global_earthquakes.html')
